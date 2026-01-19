58b675140c9dcd95f2ffcddcbf9118cd4b087782
kavindi96 <Imalsha.kavindi96@gmail.com>
2026-01-19 16:40:51 +0530

    MobileSearchPage Added.

---
+++ src/test/java/pom/pages/MobileSearchPage.java
@@ -0,0 +1,24 @@
+package pom.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class MobileSearchPage {
+
+    private WebDriver driver;
+
+    @FindBy(xpath = "//input[@id='gh-ac']")
+    private WebElement searchBox;
+
+    @FindBy(xpath = "//select[@id='gh-cat']")
+    private WebElement categoryDropDown;
+
+    @FindBy(xpath = "//button[@id='gh-search-btn']")
+    private WebElement searchButton;
+
+
+
+
+}
