43970d85ed189a40df439fe4b263c74500b04749
kavindi96 <Imalsha.kavindi96@gmail.com>
2026-01-21 15:49:07 +0530

    Update SearchTest.java

--- src/test/java/pom/tests/SearchTest.java
+++ src/test/java/pom/tests/SearchTest.java
@@ -6,29 +6,43 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pom.pages.ClothSearchPage;
 import pom.pages.MobileSearchPage;
+import pom.pages.SearchItemPage;
 import pom.utils.TreadLocalWebDriver;
 
 public class SearchTest {
-    private MobileSearchPage mobileSearchPage;
-    private ClothSearchPage clothSearchPage;
+ //   private MobileSearchPage mobileSearchPage;
+ //   private ClothSearchPage clothSearchPage;
+
+    private SearchItemPage searchItemPage;
 
     @BeforeMethod
     public void setup(){
         WebDriver driver = TreadLocalWebDriver.createWebDriver();
         driver.manage().window().maximize();
         driver.get("https://www.ebay.com/");
-        mobileSearchPage = new MobileSearchPage(driver);
-        clothSearchPage = new ClothSearchPage(driver);
+ //       mobileSearchPage = new MobileSearchPage(driver);
+ //       clothSearchPage = new ClothSearchPage(driver);
+        searchItemPage = new SearchItemPage(driver);
+    }
+
+ //   @Test
+//    public void searchForIphone(){
+ //       mobileSearchPage.searchMobile("iphone");
+ //   }
+//
+ //    @Test
+ //   public void searchForMensTShirts(){
+//        clothSearchPage.searchClothing("Men's T-Shirt");
+//    }
+
+    @Test
+    public void searchItemIphone(){
+        searchItemPage.searchItem("iPhone","Cell Phones & Accessories");
     }
 
     @Test
-    public void searchForIphone(){
-        mobileSearchPage.searchMobile("iphone");
-    }
-
-    @Test
-    public void searchForMensTShirts(){
-        clothSearchPage.searchClothing("Men's T-Shirt");
+    public void searchItemCloth(){
+        searchItemPage.searchItem("Men's t-shirt","Clothing, Shoes & Accessories");
     }
 
     @AfterMethod
